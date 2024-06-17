3d15a46572ca84a581b99401ab3a2fa0750d9d21
aliouuuw <wadealiou00@gmail.com>
2024-06-17 15:25:43 +0000

    Switched imported Spline package

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,6 @@
-import Spline from "@splinetool/react-spline/next";
+"use client"
+
+import Spline from "@splinetool/react-spline";
 import { Button } from "./ui/button";
 import { FlipWords } from "./ui/flip-words";
 
